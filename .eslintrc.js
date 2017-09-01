6e1d4cc77416bae2bf6870443c600fa47976c949
Keisuke Kan <9renpoto@gmail.com>
2017-09-02 00:46:03 +0900

    chore(eslintrc) flow activate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 /* @flow */
 module.exports = {
-  extends: ['@9renpoto/eslint-config', '@9renpoto/eslint-config-react'],
+  extends: [
+    '@9renpoto/eslint-config-flowtype',
+    '@9renpoto/eslint-config-react'
+  ],
   plugins: ['jest'],
   env: {
     'jest/globals': true
