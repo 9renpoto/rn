4b28fa01a61dc4a55773b74eb7b4fd43d39e860b
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2019-10-31 11:26:18 +0900

    feat: interface

--- packages/templates/src/Formik.tsx
+++ packages/templates/src/Formik.tsx
@@ -11,79 +11,93 @@ const validationSchema = object().shape({
   email: string().email(),
 })
 
-export const MyReactNativeForm = () => (
-  <Formik
-    initialValues={{ email: '' }}
-    onSubmit={values => {
-      Alert.alert(JSON.stringify(values))
-      console.log(values)
-    }}
-    validationSchema={validationSchema}
-  >
-    {({
-      handleChange,
-      handleBlur,
-      handleSubmit,
-      values,
-      errors,
-      isValid,
-      isSubmitting,
-      touched,
-    }) => (
-      <View>
-        <TextInput
-          placeholder="Input email address"
-          onChangeText={handleChange('email')}
-          onBlur={handleBlur('email')}
-          value={values.email}
-        />
-        <Button
-          onPress={handleSubmit}
-          title="Submit"
-          disabled={!values.email || !isValid || isSubmitting}
-        />
-        {touched.email && <Text>{errors.email}</Text>}
-      </View>
-    )}
-  </Formik>
-)
+interface FormikProps {
+  email: string
+}
 
-export const ElementsFormik = () => (
-  <Formik
-    initialValues={{ email: '' }}
-    onSubmit={values => {
-      Alert.alert(JSON.stringify(values))
-      console.log(values)
-    }}
-    validationSchema={validationSchema}
-  >
-    {({
-      handleChange,
-      handleBlur,
-      handleSubmit,
-      values,
-      errors,
-      isValid,
-      isSubmitting,
-      touched,
-    }) => (
-      <View>
-        <ElementsInput
-          label="email"
-          placeholder="Input email address"
-          onChangeText={handleChange('email')}
-          onBlur={handleBlur('email')}
-          value={values.email}
-          errorMessage={
-            touched.email && errors.email ? errors.email : undefined
-          }
-        />
-        <ElementsButton
-          onPress={handleSubmit}
-          title="Submit"
-          disabled={!values.email || !isValid || isSubmitting}
-        />
-      </View>
-    )}
-  </Formik>
-)
+export const MyReactNativeForm = () => {
+  const initialValues: FormikProps = {
+    email: '',
+  }
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={values => {
+        Alert.alert(JSON.stringify(values))
+        console.log(values)
+      }}
+      validationSchema={validationSchema}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        isValid,
+        isSubmitting,
+        touched,
+      }) => (
+        <View>
+          <TextInput
+            placeholder="Input email address"
+            onChangeText={handleChange('email')}
+            onBlur={handleBlur('email')}
+            value={values.email}
+          />
+          <Button
+            onPress={handleSubmit}
+            title="Submit"
+            disabled={!values.email || !isValid || isSubmitting}
+          />
+          {touched.email && <Text>{errors.email}</Text>}
+        </View>
+      )}
+    </Formik>
+  )
+}
+
+export const ElementsFormik = () => {
+  const initialValues: FormikProps = {
+    email: '',
+  }
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={values => {
+        Alert.alert(JSON.stringify(values))
+        console.log(values)
+      }}
+      validationSchema={validationSchema}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        isValid,
+        isSubmitting,
+        touched,
+      }) => (
+        <View>
+          <ElementsInput
+            label="email"
+            placeholder="Input email address"
+            onChangeText={handleChange('email')}
+            onBlur={handleBlur('email')}
+            value={values.email}
+            errorMessage={
+              touched.email && errors.email ? errors.email : undefined
+            }
+          />
+          <ElementsButton
+            onPress={handleSubmit}
+            title="Submit"
+            disabled={!values.email || !isValid || isSubmitting}
+          />
+        </View>
+      )}
+    </Formik>
+  )
+}
