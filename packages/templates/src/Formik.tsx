c9c1a74f3c09adcd453c23aa961f5b0e1758f0b6
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2019-10-31 10:40:37 +0900

    feat: validation

--- packages/templates/src/Formik.tsx
+++ packages/templates/src/Formik.tsx
@@ -1,20 +1,31 @@
 import React from 'react'
-import { Button, TextInput, View } from 'react-native'
+import { Button, Text, TextInput, View } from 'react-native'
 import { Formik } from 'formik'
+import { object, string } from 'yup'
+
+const validationSchema = object().shape({
+  email: string().email('email dayo'),
+})
 
 export const MyReactNativeForm = () => (
   <Formik
     initialValues={{ email: '' }}
     onSubmit={values => console.log(values)}
+    validationSchema={validationSchema}
   >
-    {({ handleChange, handleBlur, handleSubmit, values }) => (
+    {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
       <View>
         <TextInput
           onChangeText={handleChange('email')}
           onBlur={handleBlur('email')}
           value={values.email}
         />
-        <Button onPress={handleSubmit} title="Submit" />
+        <Button
+          onPress={handleSubmit}
+          title="Submit"
+          disabled={!!errors.email || !values.email}
+        />
+        <Text>{errors.email}</Text>
       </View>
     )}
   </Formik>
