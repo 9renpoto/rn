b37d0e9a60a5497b70b522378926cad22196c154
Keisuke Kan <9renpoto@gmail.com>
2019-11-15 14:48:53 +0900

    chore: try elements

--- packages/templates/src/Layout.test.tsx
+++ packages/templates/src/Layout.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import { Button } from 'react-native-elements'
 import { Layout } from './Layout'
 
 describe('Layout', () => {
   it('render', () => {
-    const wrapper = shallow(<Layout>hello</Layout>)
+    const wrapper = shallow(
+      <Layout>
+        <Button title="hello" />
+      </Layout>,
+    )
     expect(wrapper).toMatchSnapshot()
   })
 })
