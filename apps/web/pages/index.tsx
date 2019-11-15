b741ef89bec7d732eb642b0927dbfecbe078810f
Keisuke Kan <9renpoto@gmail.com>
2019-11-15 16:10:15 +0900

    :cold_sweat:

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'react-native-elements'
+import { Layout } from '@app/templates'
+
+export default function() {
+  return (
+    <Layout>
+      <Button title="hello" />
+    </Layout>
+  )
+}
